ca0784bdd45c0975de66941327824322e5bdf969
HeoGwan <idhplus948@gmail.com>
2025-08-28 11:52:22 +0900

    feat: add frame setting

--- Assets/Scripts/Managers/SettingManager.cs
+++ Assets/Scripts/Managers/SettingManager.cs
@@ -1,12 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using GameSound = CESCO.SOUND;
 using CESCO;
+using TMPro;
 
 public class SettingManager : MonoBehaviour
 {
+    public enum FrameType
+    {
+        Fast = 30,
+        Fancy = 60,
+    }
+    
     public Slider BGM;
     public Image BGMHandle;
 
@@ -18,6 +26,27 @@ public class SettingManager : MonoBehaviour
 
     [SerializeField] private List<Sprite> speakerSprites;
     private float soundStandard = 0.25f;
+
+    [Space, Header("Frame")]
+    public Button fastButton;
+    public Button fancyButton;
+    public TextMeshProUGUI fastButtonText;
+    public TextMeshProUGUI fancyButtonText;
+    
+    private FrameType _currentFrame = FrameType.Fancy;
+    private Color32 _activateColor = new(0x00, 0x00, 0x00, 0xff);
+    private Color32 _deactivateColor = new(0x98, 0x98, 0x98, 0x98);
+
+    private void Start()
+    {
+        // 기본 프레임 설정
+        if (PlayerPrefs.HasKey("Frame"))
+        {
+            _currentFrame = (FrameType)PlayerPrefs.GetInt("Frame");
+        }
+        ToggleButton();
+        SetFrame();
+    }
 
     private void ShowSetting()
     {
@@ -53,10 +82,14 @@ public class SettingManager : MonoBehaviour
 
     public void Apply()
     {
+        // 사운드 설정
         GameManager.instance.soundManager.ChangeVolume(GameSound.BGM, BGM.value);
         GameManager.instance.soundManager.ChangeVolume(GameSound.EFFECT, Effect.value);
         GameManager.instance.soundManager.ChangeVolume(GameSound.BUG_EFFECT, BugEffect.value);
 
+        // 프레임 설정
+        SetFrame();
+        
         ShowSetting();
     }
 
@@ -106,5 +139,45 @@ public class SettingManager : MonoBehaviour
         }
 
         BugEffectHandle.sprite = speakerSprites[spriteIndex];
+    }
+    
+    // =====프레임 설정=====
+    public void SetFast()
+    {
+        _currentFrame = FrameType.Fast;
+        ToggleButton();
+    }
+    
+    public void SetFancy()
+    {
+        _currentFrame = FrameType.Fancy;
+        ToggleButton();
+    }
+    
+    public void ToggleButton()
+    {
+        if (_currentFrame == FrameType.Fast)
+        {
+            fastButton.interactable = false;
+            fastButtonText.color = _deactivateColor;
+            
+            fancyButton.interactable = true;
+            fancyButtonText.color = _activateColor;
+        }
+        else if (_currentFrame == FrameType.Fancy)
+        {
+            fastButton.interactable = true;
+            fastButtonText.color = _activateColor;
+            
+            fancyButton.interactable = false;
+            fancyButtonText.color = _deactivateColor;
+        }
+    }
+    
+    private void SetFrame()
+    {
+        Application.targetFrameRate = (int)_currentFrame;
+        
+        PlayerPrefs.SetInt("Frame", (int)_currentFrame);
     }
 }
